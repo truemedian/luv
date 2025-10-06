929daee31e92aa9ea7a766bbab9f3adcda7c4064
Nameless <truemedian@gmail.com>
2025-10-06 16:24:35 -0500

    apply new changes to uv_check

---
+++ src/check.h
@@ -0,0 +1,35 @@
+/*
+ *  Copyright 2014 The Luvit Authors. All Rights Reserved.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+#ifndef LUV_CHECK_H
+#define LUV_CHECK_H
+
+#include "internal.h"
+
+LUV_LIBAPI void luv_check_init(lua_State *L);
+LUV_LIBAPI uv_check_t *luv_check_check(lua_State *L, int index);
+
+LUV_LUAAPI int luv_new_check(lua_State *L);
+LUV_LUAAPI int luv_check_start(lua_State *L);
+LUV_LUAAPI int luv_check_stop(lua_State *L);
+
+luaL_Reg luv_check_functions[] = {
+    {"new_check", luv_new_check},
+    {"check_start", luv_check_start},
+    {"check_stop", luv_check_stop},
+    {NULL, NULL},
+};
+
+#endif
